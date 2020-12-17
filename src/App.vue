fd3ff0da210fd9e8593ccc09c4f676924336841b
LucaKlingler <lucaklingler@outlook.de>
2020-12-17 10:38:34 +0100

    added sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <div id="app">
+    <b-sidebar id="sidebar-1" title="Sidebar" shadow>
+      <div class="px-3 py-2">
+        <p>
+          Dies ist eine Test-Sidebar.
+        </p>
+        <b-img src="https://i.picsum.photos/id/1025/4951/3301.jpg?hmac=_aGh5AtoOChip_iaMo8ZvvytfEojcgqbCH7dzaz-H8Y" fluid thumbnail></b-img>
+      </div>
+    </b-sidebar>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> | 
-      <router-link to="/login">Login</router-link>
+      <router-link to="/login">Login</router-link> | 
+      <span v-b-toggle.sidebar-1>Sidebar</span>
     </div>
     <router-view/>
   </div>
